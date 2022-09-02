cabdd20404a5a7f88b89fc09b438f6ae88ebccee
platelets <wenkanedward@outlook.com>
2022-09-02 20:02:24 +0800

    chang llvm-msvc to std lib

--- first.c
+++ first.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
 int main(){
-    printf("Hello world");
+    printf("Hello world\n");
+    int number;
+    // scanf("number is %d",&number);
+    scanf_s("%d",&number);
+    printf("you number is %d",number);
     return 0;
 }
