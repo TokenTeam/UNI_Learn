6fe971eceae6914684a6e771a343d5d53b77c928
platelets <wenkanedward@outlook.com>
2022-09-02 20:17:21 +0800

    show difference when use msvc and gcc

--- first.c
+++ first.c
@@ -3,8 +3,8 @@
 int main(){
     printf("Hello world\n");
     int number;
-    // scanf("number is %d",&number);
-    scanf_s("%d",&number);
+    scanf("%d",&number);
+    // scanf_s("%d",&number);
     printf("you number is %d",number);
     return 0;
 }
